c46b970d64293764370cd1577b9ef9f2578ad0f8
KarrarAbdAli <karrar.khames@gmail.com>
2025-04-18 22:26:44 +0200

    Create Empty Project

---
+++ Cocktail/Cocktail/CocktailApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailApp.swift
+//  Cocktail
+//
+//  Created by Karrar Abd Ali on 18/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct CocktailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
